173447ab1a6b1d0515755a0186c11aa3eb3cee50
chelseacastelli <chelsea.castelli@students.makeschool.com>
2019-09-12 16:27:38 -0700

    Made API call and returned response

--- app.py
+++ app.py
@@ -17,19 +17,22 @@ def index():
     params = {
         'q': query,
         'key': '4MW2HHGJDLMK',
-        'limit' 10:
+        'limit': 10
     }
-
 
     # TODO: Make an API call to Tenor using the 'requests' library. For
     # reference on how to use Tenor, see:
     # https://tenor.com/gifapi/documentation
+    r = requests.get(
+    "https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % ('q', 'key', 'limit'))
 
     # TODO: Use the '.json()' function to get the JSON of the returned response
     # object
+    gif_json = r.json()
 
     # TODO: Using dictionary notation, get the 'results' field of the JSON,
     # which contains the GIFs as a list
+    output = gif_json['results']
 
     # TODO: Render the 'index.html' template, passing the list of gifs as a
     # named parameter called 'gifs'
