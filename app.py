4f3f48befdf728c7c1f3deefdba6769d67707714
chelseacastelli <chelsea.castelli@students.makeschool.com>
2019-09-15 19:52:39 -0700

    Have GIFs show on page

--- app.py
+++ app.py
@@ -8,7 +8,9 @@ app = Flask(__name__)
 def index():
     """Return homepage."""
     # TODO: Extract the query term from url using request.args.get()
+
     query = request.args.get('search')
+    #query = 'coding'
 
     # TODO: Make 'params' dictionary containing:
     # a) the query term, 'q'
@@ -23,21 +25,19 @@ def index():
     # TODO: Make an API call to Tenor using the 'requests' library. For
     # reference on how to use Tenor, see:
     # https://tenor.com/gifapi/documentation
-    r = requests.get(
-    "https://api.tenor.com/v1/search?q=%s&key=%s&limit=%s" % ('q', 'key', 'limit'))
+    r = requests.get("https://api.tenor.com/v1/search", params=params)
 
     # TODO: Use the '.json()' function to get the JSON of the returned response
     # object
-    gif_json = r.json()
+    gifs_json = r.json()
 
     # TODO: Using dictionary notation, get the 'results' field of the JSON,
     # which contains the GIFs as a list
-    output = gif_json['results']
+    gifs = gifs_json['results']
 
     # TODO: Render the 'index.html' template, passing the list of gifs as a
     # named parameter called 'gifs'
-
-    return render_template("index.html")
+    return render_template("index.html", gifs=gifs)
 
 if __name__ == '__main__':
     app.run(debug=True)
