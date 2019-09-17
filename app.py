6887dc7596af2850880580d303f8ce52d34d1952
Student Laptop_7/19_1 <studentlaptop_719_1@Students-MacBook-Pro.local>
2019-09-17 16:58:11 -0700

    Throw An Error Message

--- app.py
+++ app.py
@@ -9,8 +9,10 @@ def index():
     """Return homepage."""
     # TODO: Extract the query term from url using request.args.get()
 
-    query = request.args.get('search')
-    #query = 'coding'
+    query = request.args.get('query')
+    print(query)
+    #Equivalent to retrieve name 'query' for a form request
+    #q = request.form['query']
 
     # TODO: Make 'params' dictionary containing:
     # a) the query term, 'q'
@@ -22,18 +24,21 @@ def index():
         'limit': 10
     }
 
+#hi
     # TODO: Make an API call to Tenor using the 'requests' library. For
     # reference on how to use Tenor, see:
     # https://tenor.com/gifapi/documentation
-    r = requests.get("https://api.tenor.com/v1/search", params=params)
+    r = requests.get("https://api.tenor.com/v1/search", params=params).json()
 
     # TODO: Use the '.json()' function to get the JSON of the returned response
     # object
-    gifs_json = r.json()
+    # gifs_json = r.json()
+    # if r.status_code == 200:
 
     # TODO: Using dictionary notation, get the 'results' field of the JSON,
     # which contains the GIFs as a list
-    gifs = gifs_json['results']
+    gifs = r['results']
+    print(gifs)
 
     # TODO: Render the 'index.html' template, passing the list of gifs as a
     # named parameter called 'gifs'
