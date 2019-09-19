04e6886781f30e35e1da34c8ca61d8edbcce4350
Student Laptop_7/19_1 <studentlaptop_719_1@Students-MacBook-Pro.local>
2019-09-19 15:22:03 -0700

    Display the Gifs instead of a List

--- app.py
+++ app.py
@@ -7,41 +7,37 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     """Return homepage."""
-    # TODO: Extract the query term from url using request.args.get()
+    # Extract the query term from url using request.args.get()
 
     query = request.args.get('query')
     print(query)
     #Equivalent to retrieve name 'query' for a form request
     #q = request.form['query']
 
-    # TODO: Make 'params' dictionary containing:
+    # Make 'params' dictionary containing:
     # a) the query term, 'q'
     # b) your API key, 'key'
     # c) how many GIFs to return, 'limit'
     params = {
         'q': query,
         'key': '4MW2HHGJDLMK',
-        'limit': 10
+        'limit': 18
     }
-
-#hi
-    # TODO: Make an API call to Tenor using the 'requests' library. For
-    # reference on how to use Tenor, see:
-    # https://tenor.com/gifapi/documentation
+#limit changed to 18 to accomodate Grid Layout
+    # Make an API call to Tenor using the 'requests' library.
+    # Tenor Reference, see: https://tenor.com/gifapi/documentation
     r = requests.get("https://api.tenor.com/v1/search", params=params).json()
 
-    # TODO: Use the '.json()' function to get the JSON of the returned response
-    # object
+    # Use the '.json()' function to get the JSON of the returned response
     # gifs_json = r.json()
     # if r.status_code == 200:
 
-    # TODO: Using dictionary notation, get the 'results' field of the JSON,
-    # which contains the GIFs as a list
+
+    # Use dict. notation with results param of JSON which contains the GIFs as a list
     gifs = r['results']
     print(gifs)
 
-    # TODO: Render the 'index.html' template, passing the list of gifs as a
-    # named parameter called 'gifs'
+    # Render the 'index.html' template, passing the list of gifs as a named parameter called 'gifs'
     return render_template("index.html", gifs=gifs)
 
 if __name__ == '__main__':
